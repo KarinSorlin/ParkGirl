82768cf2e9e95f913ad766f103727444bc873235
Ramisa <ramisa.tahsin98gmail.com>
2019-03-12 14:45:44 +0100

    Continued on the startpage

--- ParkGirl.java
+++ ParkGirl.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class ParkGirl{
 
   private void printWelcome(){
-    System.out.println(" Welcome to ParkGirl garage! \n +
-                        Our opening hours are 00.00-24.00, all week Monday to Sunday");
+    System.out.println(" Welcome to ParkGirl garage! \n" +
+                        "Our opening hours are 00.00-24.00, all week Monday to Sunday");
+  }
+
+  private void printPrice(){
+    System.out.println(" Price option: \n" +
+                        " 1 hour is 10 kr \n" +
+                        " 4 hours is 40 kr \n" +
+                        " 5-24 hours is 50 kr \n" +
+                        " 2 days is 100 kr \n" +
+                        " 2 weeks is 700 kr ");
+  }
+
+  private void printVacancies(){
+    System.out.println(" Number of vacancies: ");
   }
 
   private void printMenu(){
